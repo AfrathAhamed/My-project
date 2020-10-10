295e29541fb9eacdba15b30a21a2c07df475f8c1
AfrathAhamed <72633195+AfrathAhamed@users.noreply.github.com>
2020-10-10 11:18:42 +0530

    Add files via upload

---
+++ MyCode.py
@@ -0,0 +1,13 @@
+test1 = float(input("enter the result of test1 :"))
+test2 = float(input("enter the result of test 2 : "))
+test3 = float(input("enter the result of test# :"))
+average = (test1 + test2 + test3) / 3
+if average >=40:
+    print("Your average score is: ", average, "You are eligible to follow the course"),
+else:
+    print("Your average score is ", average, "You are not eligible to follow the course")
+
+
+
+
+
